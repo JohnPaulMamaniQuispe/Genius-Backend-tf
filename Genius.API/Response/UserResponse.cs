c8bf5ad489a94d1a7af8c6d4138910319de16abf
JohnPaulM@m@ni <u201919105@upc.edu.pe>
2023-06-26 17:34:25 -0500

    add
    secretkey

--- Genius.API/Response/UserResponse.cs
+++ Genius.API/Response/UserResponse.cs
@@ -3,5 +3,6 @@ namespace Genius.API.Response;
 public class UserResponse
 {
     public string Username { get; set; }
+    
     public string Roles { get; set; }
 }
--- Genius.Shared/Constans.cs
+++ Genius.Shared/Constans.cs
@@ -3,5 +3,5 @@
 public class Constans
 {
     public const int MaxQuantityInventory = 250;
-    public const string SecretKey = "my_genius_grupo3_secret_key";
+    public const string SecretKey = "my_geniusgeniusgenius_grupo3_secret_key";
 }
